ef35024425aba76af61a8d8732bdb5b2b37b7127
Sneha P Pratap <snehapratap248@gmail.com>
2025-03-21 12:08:17 +0530

    done

--- dags/extract.py
+++ dags/extract.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 def extract_data():
-    csv_data = pd.read_csv("/opt/airflow/dags/data/train_schedule.csv")
-    excel_data = pd.read_excel("/opt/airflow/dags/data/train_schedule.xlsx")
+    csv_data = pd.read_csv("/data/train_schedule.csv")
+    excel_data = pd.read_excel("/data/train_schedule.xlsx")
+    json_data = pd.read_json("/data/train_schedule.json")
+
     return pd.concat([csv_data, excel_data], ignore_index=True)
